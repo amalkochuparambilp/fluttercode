be13e0d8b8f67dce335fc18ecd4f88eb5f556660
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2024-10-28 11:17:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 if (function_exists('ini_set')) @ini_set('opcache.enable', '0');
-include dirname(__FILE__).'/sitepro/appear-free/index.html';
+include dirname(__FILE__).'/web/index.html';
 ?>
